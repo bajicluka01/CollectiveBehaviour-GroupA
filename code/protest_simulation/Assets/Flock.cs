0ab287bed9953c72359d45c358bcbe5bd69cb734
Luka Bajić <bajicluka01@gmail.com>
2024-11-24 23:42:02 +0100

    Added agent movement

--- code/protest_simulation/Assets/Flock.cs
+++ code/protest_simulation/Assets/Flock.cs
@@ -48,6 +48,27 @@ public class Flock : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        foreach(FlockAgent agent in agents) {
+            List<Transform> context = GetNearbyObjects(agent);
+            //agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.red, context.Count / 6f);
+
+            Vector2 move = behavior.CalculateMove(agent, context, this);
+            move*=driveFactor;
+            if (move.sqrMagnitude > squareMaxSpeed) {
+                move = move.normalized * maxSpeed;
+            }
+            agent.Move(move);
+        }
+    }
+
+    List<Transform> GetNearbyObjects(FlockAgent agent) {
+        List<Transform> context = new List<Transform>();
+        Collider2D[] contextColliders = Physics2D.OverlapCircleAll(agent.transform.position, neighborRadius);
+        foreach(Collider2D c in contextColliders) {
+            if (c != agent.AgentCollider) {
+                context.Add(c.transform);
+            }
+        }
+        return context;
     }
 }
---
+++ code/protest_simulation/Assets/behavior_scripts/CompositeBehavior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Composite")]
+public class CompositeBehavior : FlockBehavior {
+
+    public FlockBehavior[] behaviors;
+    public float[] weights;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock) {
+        
+        if(weights.Length != behaviors.Length) {
+            Debug.LogError("Data mismatch in "+name, this);
+            return Vector2.zero;
+        }
+
+        Vector2  move = Vector2.zero;
+
+        for(int i = 0; i < behaviors.Length; i++) {
+            Vector2 partialMove = behaviors[i].CalculateMove(agent, context, flock) * weights[i];
+            
+            if (partialMove != Vector2.zero) {
+                if (partialMove.sqrMagnitude > weights[i] * weights[i]) {
+                    partialMove.Normalize();
+                    partialMove *= weights[i];
+                }
+
+                move += partialMove;
+            }
+        }
+
+        return move;
+    }
+}
