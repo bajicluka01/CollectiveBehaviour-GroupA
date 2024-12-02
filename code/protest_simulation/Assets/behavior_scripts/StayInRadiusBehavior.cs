955b72f63ffc20f0aa09e73f5a16b3ecb8c4b036
Luka Bajić <bajicluka01@gmail.com>
2024-12-02 13:22:39 +0100

    Removed the unnecessary duplicate camera

--- code/protest_simulation/Assets/behavior_scripts/StayInRadiusBehavior.cs
+++ code/protest_simulation/Assets/behavior_scripts/StayInRadiusBehavior.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 public class StayInRadiusBehavior : FlockBehavior {
 
     public Vector2 center;
-    public float radius = 15f;
+    public float radius = 100f;
     
     public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock) {
         Vector2 centerOffset = center - (Vector2)agent.transform.position;
