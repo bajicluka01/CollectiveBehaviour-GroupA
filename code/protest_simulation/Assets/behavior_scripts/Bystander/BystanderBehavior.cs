2b17346df6389df4032b630eb37084565f070150
root_rooster <gmail@nikcadez.com>
2024-12-06 14:22:21 +0100

    updated scripts

--- code/protest_simulation/Assets/behavior_scripts/Bystander/BystanderBehavior.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BystanderBehavior : ScriptableObject {
-    public abstract Vector2 CalculateMove (BystanderAgent agent, List<Transform> context, BystanderFlock flock); 
-
-    
-}
--- code/protest_simulation/Assets/behavior_scripts/Protester/ProtesterFilteredFlockBehavior.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public abstract class ProtesterFilteredFlockBehavior : ProtesterBehavior {
-        public ProtesterContextFilter filter;
-}
