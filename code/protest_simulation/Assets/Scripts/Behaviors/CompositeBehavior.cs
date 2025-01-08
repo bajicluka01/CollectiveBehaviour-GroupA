b5ef8bd8a4636e8eb21b2f323756010c3ab85b8a
Nik <62603376+RootRooster@users.noreply.github.com>
2025-01-08 21:54:15 +0100

    removed empty lines for clairty

--- code/protest_simulation/Assets/Scripts/Behaviors/CompositeBehavior.cs
+++ code/protest_simulation/Assets/Scripts/Behaviors/CompositeBehavior.cs
@@ -9,15 +9,12 @@ public class CompositeBehavior : FlockBehavior
 
     public override Vector2 CalculateMove(FlockAgent agent, Flock flock)
     {
-
         if (weights.Length != behaviors.Length)
         {
             Debug.LogError("Data mismatch in " + name, this);
             return Vector2.zero;
         }
-
         Vector2 move = Vector2.zero;
-
         for (int i = 0; i < behaviors.Length; i++)
         {
             Vector2 partialMove = behaviors[i].CalculateMove(agent, flock) * weights[i];
@@ -28,10 +25,10 @@ public class CompositeBehavior : FlockBehavior
                     partialMove.Normalize();
                     partialMove *= weights[i];
                 }
-
                 move += partialMove;
             }
         }
+        // TODO: Figure out if we really need this desired speed?
         return move.normalized*agent.DesiredSpeed;
     }
 }
